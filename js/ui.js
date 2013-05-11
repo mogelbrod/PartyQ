35f728787bd6ea0a20b143cc3573d576133d3675
Victor Hallberg <vigge19@gmail.com>
2013-05-11 14:10:30 +0200

    Update ui code, tweak table markup

--- js/ui.js
+++ js/ui.js
@@ -6,8 +6,13 @@ $(function() {
 
   var tracksURIs = [
     "spotify:track:4yJmwG2C1SDgcBbV50xI91",
+    "spotify:track:4yJmwG2C1SDgcBbV50xI91",
     "spotify:track:4uwaTTrDykHbWFBb8RGYWI",
     "spotify:track:2Foc5Q5nqNiosCNqttzHof",
+    "spotify:track:4uwaTTrDykHbWFBb8RGYWI",
+    "spotify:track:4uwaTTrDykHbWFBb8RGYWI",
+    "spotify:track:4yJmwG2C1SDgcBbV50xI91",
+    "spotify:track:4yJmwG2C1SDgcBbV50xI91",
     "spotify:track:4yJmwG2C1SDgcBbV50xI91"
   ];
 
@@ -18,42 +23,66 @@ $(function() {
     return uri.replace(/^spotify:track:/, '');
   }
 
+  function createTrackRow(data) {
+    var $row = $('<div>').html(trackRowTemplate(data)).contents();
+    $row.data('requests', data.requests);
+    $row.data('uri', data.uri);
+    return $row;
+  }
+
   for (var i = 0; i < tracksURIs.length; i++) {
     var t = tracksURIs[i];
-    onRequest(t, null);
+    onRequest(t);
   };
 
   /**
    * Remote request callback function
    */
-  function onRequest(track, requestData) {
-    var data = {
+  function onRequest(track, data, tweetData) {
+    var vars = {
       id: trackID(track),
       uri: track,
-      artists: [{ name: 'Unknown' }],
+      artists: [
+        { name: 'Unknown', uri: 'spotify:artist:518rTAIFPwQjLUSi4Pdzzn' }
+      ],
       title: track,
       requests: 1,
-      requestedBy: 'Anonymous'
+      requesters: [
+        { user: 'Anonymous' }
+      ]
     };
+
     $emptyRow.hide();
 
-    var $existing = $('#'+data.id);
+    var $existing = $('#' + vars.id);
     // Track exists in list, increment request count
     if ($existing.length) {
-      var $td = $existing.find('td.requests');
-      var num = parseInt($td.text());
-      num += 1;
-      $td.text(num);
+      vars.requests = $existing.data('requests') + 1;
+      console.log(vars);
+
+      // Find new position for track
+      var $prevAll = $existing.prevAll();
+      var i = $prevAll.length - 1;
+      for (; i > 0; --i) {
+        var prevNum = parseInt($prevAll.eq(i).data('requests'));
+        if (prevNum > vars.requests)
+          break;
+      }
+      // Reposition track
+      $existing.slideUp(function() {
+        $(this).remove();
+        createTrackRow(vars).insertBefore($prevAll.eq(i));
+      });
     }
     // New request (track does not exist in list)
     else {
-      $rows.append(trackRowTemplate(data));
+      createTrackRow(vars).appendTo($rows);
     }
   }
 
   $rows.on('dblclick', 'tr', function(event) {
     var $row = $(this);
-    console.log($row.attr('id'));
+    console.log($row.data('uri'));
     $row.remove();
     event.preventDefault();
     return false;
