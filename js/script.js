a64462b565a0822ff3283d0d73b847bf8e8fe46f
Simon Strom <simon.strom@gmail.com>
2013-05-11 09:33:07 +0200

    merge

--- js/script.js
+++ js/script.js
@@ -1,36 +1,15 @@
 'use strict';
+window.onload = function() {
+  var sp = getSpotifyApi();
+  var models = sp.require('$api/models');
 
-var sp = getSpotifyApi(1),
-    trackInfo = document.getElementById('track-info');
-
-
-function updateTrackDetails() {
-  var playerTrackInfo = sp.trackPlayer.getNowPlayingTrack(),
-      infoHTML = '',
-      track;
-  if (playerTrackInfo == null) {
-    infoHTML = 'Nothing playing!';
-  } else {
-    track = playerTrackInfo.track;
-    console.log(playerTrackInfo);
-    console.log(track);
-    infoHTML = '<span id="track-title">' + track.name + '</span>' +
-      'on the album <span id="track-album">' + track.album.name + '</span>' +
-      'by <span id="track-artist">' + track.album.artist.name + '</span>';
+  function playTracks(tracks, index) {
+    var pl = new models.Playlist();
+    pl.add("spotify:track:6JEK0CvvjDjjMUBFoXShNZ");
+    
+    models.player.play(pl.uri, pl.uri, 0);
   }
 
-  trackInfo.innerHTML = infoHTML;
-}
+  playTracks("", 0);
 
-(function () {
-  console.log('init()');
-
-  updateTrackDetails();
-
-  sp.trackPlayer.addEventListener('playerStateChanged', function (event) {
-    // Only update the page if the track changed
-    if (event.data.curtrack == true) {
-        updateTrackDetails();
-    }
-  });
-})();
+};
