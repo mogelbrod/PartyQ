8237d2bd53f5ed7f70a9fa7d8591c85c37a9b3bc
Simon Strom <simon.strom@gmail.com>
2013-05-11 13:39:44 +0200

    use temp playlist instead

--- js/script.js
+++ js/script.js
@@ -8,48 +8,96 @@ require([
   '$views/image#Image'
   ], function(models, Location, Search, Toplist, buttons, List, Image) {
 
-  /* 
-   Triggers changeTrack() when track is changed. Logic!
-  */
-  models.player.addEventListener('change:track', changeTrack);
+  var tmpPlaylist  = null;
+  var USE_PLAYLIST = true;
+  
+  // Triggers changeTrack() when track is changed. Logic!
+  models.player.addEventListener('change:track', changeEventHandler);
+
+  createPlaylist();
+
+  // Called when track is changed.
+  function changeEventHandler() {
+    if(USE_PLAYLIST)
+      addTrackToPlaylist();
+    else
+      changeTrackNow();
+  }
 
   /**
-   * called on change:track.
-   * should return the uri for next song, 
+   * Returns the uri for next song, 
    * if null the queue is used as normal
+   * TODO: This should be changed to use our queue.
    */
-  function getNextSongURI() {
+  function __getNextSongURI() {
     uri = $("#nextTrack").val();
     return uri;
   }
 
   /*
-    Fetches uri and plays that track, if uri is not null.
+    Return the next trackobject to be played.
   */
-  function changeTrack() {
-    console.log("ChangeTrack");
-    uri = getNextSongURI();
-
-    if (uri == null) {
-      // No new song
-      console.log("Dont change song. Song is null");
-    } else {
-      console.log("Setting next song: " + uri);
-      track = models.Track.fromURI(uri).load('name').done(
-        function(track) {
-          displayTrack(track);
-          playSong(track);
+  function getNextTrack(callback) {
+    uri = __getNextSongURI();
+    if (uri != null) {
+      //console.log("Setting next song: " + uri);
+      models.Track.fromURI(uri).load('name','uri').done(function(track) {
+        callback(track);
       });
     }
-
-    //updateCurrentTrack();
   }
 
+  /*
+    Fetches uri and plays that track, if uri is not null.
+  */
+  function changeTrackNow() {
+    console.log("ChangeTrack");
+    getNextTrack(function(track) {
+      console.log("changing song now: " + uri);
+      displayTrack(track);
+      playSong(track);
+    });
+  }
+
+  /*
+   * Initailly set up a playlist
+  */
+  function createPlaylist() {
+    console.log("Create custom playlist");
+    p = models.Playlist.createTemporary("CustomList"); //TODO change name
+    p.done(function(pList){
+      console.log("done. The playlist is ours");
+      pList.load('tracks').done(function(loadedPlaylist){
+        console.log("loaded");
+        tmpPlaylist = loadedPlaylist;
+      });
+    });
+  }
+
+  /*
+    Adds next track to our playlist.
+  */
+  function addTrackToPlaylist() {    
+    console.log("playlist " + tmpPlaylist.name);
+    console.log("loaded");
+    getNextTrack(function(track) {
+      console.log("got track: " + track);
+      console.log("loaded a track");
+      console.log(track);
+      tmpPlaylist.tracks.add(track);
+    }); 
+
+    // set the playlist as playing now
+    models.player.playContext(tmpPlaylist);
+    console.log("context set");
+  }
   
+  /*
+    Plays the song imidiatly.
+  */
   function playSong(track) {
     models.player.playTrack(track);
   }
-  
 
   function displayTrack(track) {
     console.log(track.name);
